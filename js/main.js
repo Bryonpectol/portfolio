d9aa04e82f715ef95cc4604c036cdbc6755f9a16
Bryon <bryon.pectol@gmail.com>
2020-10-13 17:56:00 -0600

    testing age but fail

--- js/main.js
+++ js/main.js
@@ -17,24 +17,24 @@ function clickUp() {
 
 
 
-function getAge(DOB) {
-    var today = new Date();
-    var birthDate = new Date(DOB);
-    var age = today.getFullYear() - birthDate.getFullYear();
-    var m = today.getMonth() - birthDate.getMonth();
+// function getAge(DOB) {
+//     var today = new Date();
+//     var birthDate = new Date(DOB);
+//     var age = today.getFullYear() - birthDate.getFullYear();
+//     var m = today.getMonth() - birthDate.getMonth();
 
-    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-        age = age - 1;
-        let Age = getAge("5/8/1999");
-    }
+//     if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+//         age = age - 1;
+//         let Age = getAge("5/8/1999");
+//     }
 
-    return age;
-}
-
-
-let AGE = document.write(getAge("5/8/1999"));
-let myAge = document.getElementById("age").innerHTML = Date();
+//     return age;
+// }
 
 
-myAge = AGE;
-console.log(getAge("5/8/1999") + " : " + AGE);
+// let AGE = document.write(getAge("5/8/1999"));
+// let myAge = document.getElementById("age").innerHTML = Date();
+
+
+// myAge = AGE;
+// console.log(getAge("5/8/1999") + " : " + AGE);
