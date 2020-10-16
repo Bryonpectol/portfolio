670dabf2b99b8f61f37258cf745aae38fe3931ee
Bryon <bryon.pectol@gmail.com>
2020-10-15 19:15:53 -0600

    added experience

--- js/bio.js
+++ js/bio.js
@@ -24,11 +24,6 @@ let First = document.getElementById("intro");
 let Second = document.getElementById("second");
 let Third = document.getElementById("third");
 
-let Br1 = document.getElementById("br1");
-let Br2 = document.getElementById("br2");
-let Br3 = document.getElementById("br3");
-let Br4 = document.getElementById("br4");
-
 First.innerHTML = "Hello, my name is Bryon Pectol. I am a student, studying at Utah Valley University. My major is Web and App Development.";
-Second.innerHTML = "My goals as a Front End Developer:" + Br1 + Br2 + "I plan to get my Bachelor's degree from UVU and hopefully nail my first job in the development industry. I will then work on getting my graduate degree as I am working as an entry level Front End Developer. I hope that with my graduate degree I can become a DevOps and work within the government for web safety and security.";
-Third.innerHTML = "A little more about me:" + Br3 + Br4 + "I am " + getAge("5/8/1999") + " years old. I currently live in Orem, Utah, USA. I come from a big family of 8 including me. I have quite a few interests and hobbies. My favorite hobby to pass the time is developing websites or building video games. I also really like 3D design. Some outdoor activities I like are mountain biking, snowboarding, and I like to play basketball or soccer. I also served a mission for the Church of Jesus Christ of Latter Day Saints in 2018. I served in Guayaquil, Ecuador, and it was there that I learned to speak Spanish. I am currently employed at RC Willey as a technology salesperson. I got married to my wife, Makenna, in January 2020.";
+Second.innerHTML = "I plan to get my Bachelor's degree from UVU and hopefully nail my first job in the development industry. I will then work on getting my graduate degree as I am working as an entry level Front End Developer. I hope that with my graduate degree I can become a DevOps and work within the government for web safety and security.";
+Third.innerHTML = "I am " + getAge("5/8/1999") + " years old. I currently live in Orem, Utah, USA. I come from a big family of 8 including me. I have quite a few interests and hobbies. My favorite hobby to pass the time is developing websites or building video games. I also really like 3D design. Some outdoor activities I like are mountain biking, snowboarding, and I like to play basketball or soccer. I also served a mission for the Church of Jesus Christ of Latter Day Saints in 2018. I served in Guayaquil, Ecuador, and it was there that I learned to speak Spanish. I am currently employed at RC Willey as a technology salesperson. I got married to my wife, Makenna, in January 2020.";
