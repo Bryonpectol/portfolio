c9b192c6ec868f02be31825dc93c9a92eb909cdc
Bryon <bryon.pectol@gmail.com>
2020-10-20 21:30:11 -0600

    added avatars change every month

---
+++ js/avatardata.js
@@ -0,0 +1,1 @@
+export const avatars = ["/images/icons/january.png", "/images/icons/february.png", "/images/icons/march.png", "/images/icons/april.png", "/images/icons/may.png", "/images/icons/june.png", "/images/icons/july.png", "/images/icons/august.png", "/images/icons/september.png", "/images/icons/october.png", "/images/icons/november.png", "/images/icons/december.png"];
