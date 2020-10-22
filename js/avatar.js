d484d8c2d9fc46ddb0000a22024fc3435b13f683
Bryon <bryon.pectol@gmail.com>
2020-10-21 22:12:50 -0600

    changed vars on avatar.js to lets

--- js/avatar.js
+++ js/avatar.js
@@ -1,8 +1,8 @@
 const avatars = ["/images/icons/january.png", "/images/icons/february.png", "/images/icons/march.png", "/images/icons/april.png", "/images/icons/may.png", "/images/icons/june.png", "/images/icons/july.png", "/images/icons/august.png", "/images/icons/september.png", "/images/icons/october.png", "/images/icons/november.png", "/images/icons/december.png"];
 
 
-var d = new Date();
-var n = d.getMonth();
+let d = new Date();
+let n = d.getMonth();
 
 let dateint = n;
 
